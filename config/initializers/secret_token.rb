04173b4d95e95986d9f0f8de9d5702e628954ade
Jon Prins <jonprins@gmail.com>
2011-07-23 14:22:29 -0400

    rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tinyboxes::Application.config.secret_token = '2fd19c2815bff075529b95efc6fffb613b812d1291b08f524ea586388edc20caed807d36116b77a705120c9f3af15302ed8acff2433906becb14c7faa25f1734'
